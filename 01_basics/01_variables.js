67b30f52c8f1d724da7d99bc658d8ae8ad87de23
varuni2006 <varunisrivastava024@gmail.com>
2025-06-18 10:17:21 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1111
+let accountEmail = "hitest@google.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+//accountId = 2
+//not correct
+
+console.log(accountId);
+accountEmail = " hello@gmail.com"
+accountPassword = "11111";
+accountCity = " bengalore"
+console.table([accountId, accountEmail, accountPassword, accountCity])
