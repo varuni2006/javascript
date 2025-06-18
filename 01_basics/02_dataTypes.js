4a00ec35315fb158c1c924f48e2464e3b9e9de3a
varuni2006 <varunisrivastava024@gmail.com>
2025-06-18 10:42:20 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all js code as newer version
+
+alert(3+3) // we are sng nodejs , not browser so this will not work
+
+console.log(3+3) 
+console.log("Hello")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
