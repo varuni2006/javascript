7dfc298df3507ce452356e8a83b93a4c8e9c9be8
varuni2006 <varunisrivastava024@gmail.com>
2025-06-19 11:50:38 +0000

    primitive and Non Primitive

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -17,7 +17,44 @@ console.log(typeof(newno)); // number
 //but is 33abc a number? -> NO
  console.log(newno); //NaN means it is not a number
  
+//********************************************************* */
 
+// Stack and Heap
+/*
+Stack(Primitive) , Heap(Non Primitive)
+(copy is made)     (reference is given i.e is the original value)
+
+
+*/
+
+//this is primitive example
+
+let myname = "Varuni"
+let anothername = myname
+
+anothername = "Srivastava"
+
+console.log(myname); // Varuni
+console.log(anothername); //Srivastava
+
+//in primtive type a copy is passed that is why
+//changing the value of 'anothername' does not change the value of 'myname'
+
+//*** */
+
+//example of non primitve type where passed by reference is used
+let userOne = {
+    name: "Varuni",
+    id: 123
+}
+
+let userTwo = userOne
+userTwo.name = "Srivastava"
+console.log(userOne.name); // Srivastava
+console.log(userTwo.name); // Srivastava
+
+//here by reference is used so value of name is changed at bot the places
+ 
 
 
 
